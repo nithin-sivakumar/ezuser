b7b670dda336620721bb75f5578f2148fc0b8328
Nithin <nithinsgayathri@gmail.com>
2024-01-02 18:12:45 +0530

    Deprecated functions

--- src/controllers/verifyPassword.js
+++ src/controllers/verifyPassword.js
@@ -1,15 +1,15 @@
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 
-/**
- * Validate a password using bcrypt.
- * @param {string} password - Plain text password.
- * @param {string} hashedPassword - Hashed password.
- * @returns {Promise<Boolean>} - Resolves with a boolean indicating password validity.
- */
-async function verifyPassword(password, hashedPassword) {
-  return bcrypt.compare(password, hashedPassword);
-}
+// /**
+//  * Validate a password using bcrypt.
+//  * @param {string} password - Plain text password.
+//  * @param {string} hashedPassword - Hashed password.
+//  * @returns {Promise<Boolean>} - Resolves with a boolean indicating password validity.
+//  */
+// async function verifyPassword(password, hashedPassword) {
+//   return bcrypt.compare(password, hashedPassword);
+// }
 
-module.exports = {
-  verifyPassword,
-};
+// module.exports = {
+//   verifyPassword,
+// };
